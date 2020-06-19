05bf7e2a5bf848c0357c925c343a8bb97bf15855
yasutaka.adachi <ssgut75@gmail.com>
2020-06-19 08:11:52 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,10 +19,20 @@ class UserController extends Controller
         
     public function show($id)
     {
+        // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
-        
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
-            ]);
+            'microposts' => $microposts,
+        ]);
     }
+
 }
